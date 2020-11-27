0820c11c92ae92352409883144e760839a8cf5fd
Konstantinos <Mantzavelas>
2020-11-27 14:56:02 +0200

    - Implement the front end - Minor changes in controllers and services

--- src/main/java/com/mantzavelas/jokespringapp/controllers/JokeController.java
+++ src/main/java/com/mantzavelas/jokespringapp/controllers/JokeController.java
@@ -1,22 +1,26 @@
 package com.mantzavelas.jokespringapp.controllers;
 
 import com.mantzavelas.jokespringapp.services.JokeService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class LandingController {
+public class JokeController {
 
     private final JokeService jokeService;
 
-    public LandingController(JokeService jokeService) {
+    public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
 
     @GetMapping
-    public String getJoke() {
-        return jokeService.getRandomJoke();
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
     }
 }
--- src/main/java/com/mantzavelas/jokespringapp/services/JokeService.java
+++ src/main/java/com/mantzavelas/jokespringapp/services/JokeService.java
@@ -1,18 +1,6 @@
 package com.mantzavelas.jokespringapp.services;
 
-import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.stereotype.Service;
+public interface JokeService {
 
-@Service
-public class JokeService {
-
-    private final ChuckNorrisQuotes quotes;
-
-    public JokeService(ChuckNorrisQuotes quotes) {
-        this.quotes = quotes;
-    }
-
-    public String getRandomJoke() {
-        return quotes.getRandomQuote();
-    }
+    String getJoke();
 }
